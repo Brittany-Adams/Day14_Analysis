d46b209f20b4a6cb0ca3b66aeaa59a46aeedfa9d
Brittany-Adams <brittanyadams2017@gmail.com>
2022-11-16 14:52:25 -0500

    data cleaning

--- source/01_DataCleaning.R
+++ source/01_DataCleaning.R
@@ -5,12 +5,12 @@ library(dplyr)
 dat <- read_csv("data/F2022-NHIS-MentalHealth_raw.csv")
 
 dat <- dat %>% 
-  replace_with_na_at(.vars = c("HRSLEEP"), condition = ~.x == 0)
+  replace_with_na_at(.vars = c("HRSLEEP"), condition = ~.x %in% c(00, 97, 98, 99 ))
 
 dat$sleep_mt7hr <- ifelse(dat$HRSLEEP < 7, TRUE, FALSE)
 
 dat <- dat %>% 
-  replace_with_na_at(.vars = c("AEFFORT", "AHOPELESS", "ANERVOUS", "ARESTLESS", "ASAD", "AWORTHLESS"), condition = ~.x == 0)
+  replace_with_na_at(.vars = c("AEFFORT", "AHOPELESS", "ANERVOUS", "ARESTLESS", "ASAD", "AWORTHLESS"), condition = ~.x %in% c(6, 7, 8, 9))
 
 dat <- dat %>% 
   mutate(kessler_score = AEFFORT + AHOPELESS + ANERVOUS + ARESTLESS + ASAD + AWORTHLESS)
@@ -18,7 +18,10 @@ dat <- dat %>%
 dat$mentalhealth_severe <- ifelse(dat$kessler_score < 13, FALSE, TRUE)
 
 dat <- dat %>% 
-  replace_with_na_at(.vars = c("AGE", "POORYN"), condition = ~.x == 0)
+  replace_with_na_at(.vars = c("AGE"), condition = ~.x %in% c(997, 998, 999))
+
+dat <- dat %>% 
+  replace_with_na_at(.vars = c("POORYN"), condition = ~.x == 9)
 
 write.csv(x = dat, file = "MentalHealth_clean.csv", row.names = FALSE)
 
